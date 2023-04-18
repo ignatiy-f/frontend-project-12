670acb9904f5b1dd3f1ef8a185a008094ef13b1f
Jean Green <ignatiysecond@gmail.com>
2023-04-18 23:46:08 +0300

    first step

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './components/pages/LoginPage';
+import NotFoundPage from './components/pages/notFoundPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-
-        Hello world /n
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/pages/LoginPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LoginPage = () => {
+  return (
+    <div className="d-flex flex-column h-100">
+      <div className="text-center">
+        <h1 className="h4 text-muted">{'Форма для входа'}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
